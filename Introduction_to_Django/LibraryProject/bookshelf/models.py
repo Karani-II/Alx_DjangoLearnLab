3885db3cecd5da44ef90edfbab14b62c69c0014a
Karani <karanik509@gmail.com>
2025-08-31 10:57:23 +0300

    Django_Admin

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-class Book(models.Models):
+class Book(models.Model):
     title = models.CharField(max_length = 200)
     author = models.CharField(max_length = 100)
     publication_year = models.IntegerField()
