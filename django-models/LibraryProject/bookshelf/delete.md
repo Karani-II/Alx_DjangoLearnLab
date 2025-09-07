bd6269d3ad01d37b13fd49af0e2fa7ac6658f15f
Karani <karanik509@gmail.com>
2025-09-07 15:26:47 +0300

    Django_models

---
+++ django-models/LibraryProject/bookshelf/delete.md
@@ -0,0 +1,3 @@
+from bookshelf.models import Book 
+book.models.get(title = "Nineteen Eighty_Four")
+book.delete()
---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+class Author(models.Model):
+    name = models.CharField(max_length = 200)
+class Book(models.Model):
+    title = models.CharField(max_length = 200)
+    Author = models.ForeignKey(Author , on_delete=models.CASCADE, related_name='books')
+class Library(models.Model):
+    name = models.CharField(max_length = 200)
+    books = models.ManyToManyField(Book, related_name= 'books')
+class Librarian(models.Model):
+    name = models.CharField(max_length = 200)
+    Library = models.OneToOneField(Library , on_delete=models.CASCADE, related_name = 'library')
+# Create your models here.
