91d9d4a1394e16ba83c84716a17c4f54bda8e1d4
Karani <karanik509@gmail.com>
2025-09-14 11:02:07 +0300

    Advanced features

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser 
 class Book(models.Model):
     title = models.CharField(max_length = 200)
     author = models.CharField(max_length = 100)
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import AbstractUser
 class Author(models.Model):
     name = models.CharField(max_length = 200)
     def __str__(self):
@@ -21,6 +22,10 @@ class Librarian(models.Model):
     Library = models.OneToOneField(Library , on_delete=models.CASCADE, related_name = 'library')
     def __str__(self):
         return self.name
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(blank=True null=True)
+    profile_photo = models.ImageField()
+
 class UserProfile(models.Model):
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
