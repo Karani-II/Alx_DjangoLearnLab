1c958404350a06d9970e6c0ad1da96f5ea5fb66b
Karani <karanik509@gmail.com>
2025-09-14 14:37:46 +0300

    Advanced features

--- advanced_features_and_security/LibraryProject/relationship_app/views.py
+++ advanced_features_and_security/LibraryProject/relationship_app/views.py
@@ -3,10 +3,12 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, get_object_or_404 ,redirect 
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required, user_passes_test
 from .models import Book
 from .models import Library
+from .forms import BookForm
 
 def list_books(request):
     books = Book.objects.all()
@@ -15,6 +17,36 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("book_list")
+    else:
+        form = BookForm()
+    return render(request, "relationship_app/book_form.html", {"form": form})
+
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect("book_list")
+    else:
+        form = BookForm(instance=book)
+    return render(request, "relationship_app/book_form.html", {"form": form})
+
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        book.delete()
+        return redirect("book_list")
+    return render(request, "relationship_app/book_confirm_delete.html", {"book": book})
 def home(request):
     return HttpResponse("Welcome to the Library Home Page!")
 def register(request):
