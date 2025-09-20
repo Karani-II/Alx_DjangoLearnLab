a368d42746db3968d945a87704fb9b4a702a791c
Karani <karanik509@gmail.com>
2025-09-20 18:03:38 +0300

    API_development

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Book 
-class BookSerializer(serializers.Modelserializers):
+class BookSerializer(serializers.ModelSerializers):
     class meta:
         model = Book 
         fiels = ['title','author','creation_date']
