8a78d77c2b046f30ff121c3578dde2fc1f487ade
Karani <karanik509@gmail.com>
2025-09-20 16:29:36 +0300

    api_development

---
+++ api_project/api_project/api/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    creation_date = models.DateTimeField(auto_now_add=True)
+# Create your models here.
