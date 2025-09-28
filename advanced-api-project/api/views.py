748045f4225fb3f6a8cd5204704e18262cbfd5f2
Karani <karanik509@gmail.com>
2025-09-28 14:07:06 +0300

    Advanced Api

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics , permissions 
+from .models import Book 
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
+class ListView(generics.ListApiView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer 
+    permission_classes = [permissions.AllowAny]
 
+class DetailView(generics.RetrieveApiView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+class CreateView(generics.CreateApiView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+class UpdateView(generics.UpdateApiView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+class DeleteView(generics.DestroyApiView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer 
+    permission_classes = [IsAuthenticated]
 # Create your views here.
