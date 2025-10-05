4894c52d15bf6a68420987ba80a0a15244ec79fb
Karani <karanik509@gmail.com>
2025-10-05 15:39:26 +0300

    Authentication

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class Registration(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
 
 # Create your views here.
