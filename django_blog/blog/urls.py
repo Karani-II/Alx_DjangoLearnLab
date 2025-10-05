cdb3f50bacad3a7c7332a103f60aeb8fd091e7c7
Karani <karanik509@gmail.com>
2025-10-05 22:51:44 +0300

    Comment

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
-    path('posts/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment_create'),
     path('comments/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
     path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 
