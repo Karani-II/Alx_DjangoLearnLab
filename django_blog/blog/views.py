2ed55b12926f3226d1c6244ed90788d3fa6c39b1
Karani <karanik509@gmail.com>
2025-10-05 16:11:25 +0300

    Authentication

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 class Registration(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -13,7 +14,31 @@ class Registration(UserCreationForm):
 
 @login_required
 def profile(request):
-    return render(request, 'profile.html')
+    profile, created = Profile.objects.get_or_create(user=request.user)
+
+    if request.method == 'POST':
+        new_email = request.POST.get('email')
+        new_bio = request.POST.get('bio')
+        new_picture = request.FILES.get('profile_picture')
+
+        if new_email:
+            request.user.email = new_email
+            request.user.save()
+
+        if new_bio:
+            profile.bio = new_bio
+
+        if new_picture:
+            profile.profile_picture = new_picture
+
+        profile.save()
+        return redirect('profile') 
+
+    context = {
+        'profile': profile
+    }
+    return render(request, 'userprofile.html', context)
+
 
 
 
