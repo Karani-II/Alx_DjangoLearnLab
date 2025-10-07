b7f95b20024af7b431dfe4f05a72fe891c2ffdf9
Karani <karanik509@gmail.com>
2025-10-07 13:42:36 +0300

    social_media_api

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -6,14 +6,14 @@ User = get_user_model()
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password']
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,38 +1,45 @@
-from django.shortcuts import render 
-from rest_framework import generics, status 
-from rest_framework.response import Response 
-from rest_framework.permissions import AllowAny 
-from rest_framework.authtoken.models import Token 
-from django.contrib.auth import authenticate 
-from .models import CustomUser 
-from .serializers import RegisterSerializer, CustomUserSerializers
+from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from .models import CustomUser
+from .serializers import RegisterSerializer, CustomUserSerializers 
 
-class RegisterView(generics.CreateAPIView): 
-    queryset = CustomUser.objects.all() 
-    serializer_class = RegisterSerializer 
+
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
 
-def create(self, request, *args, **kwargs): 
-    serializer = self.get_serializer(data=request.data)
-    serializer.is_valid(raise_exception=True) 
-    user = serializer.save() 
-    token, _ = Token.objects.get_or_create(user=user) 
-    return Response({ 
-        "user": CustomUserSerializers(user).data, 
-        "token": token.key 
+    def create(self, request, *args, **kwargs):  
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({
+            "user": CustomUserSerializers(user).data,
+            "token": token.key
         }, status=status.HTTP_201_CREATED)
 
-class LoginView(generics.GenericAPIView): 
-    permission_classes = [AllowAny] 
-    def post(self, request, *args, **kwargs): 
-        username = request.data.get('username') 
-        password = request.data.get('password') 
-        user = authenticate(username=username, password=password) 
-        if user: 
-           token, _ = Token.objects.get_or_create(user=user) 
-        return Response({ 
-            "token": token.key, 
-            "user": CustomUserSerializers(user).data 
-        }) 
-    return Response({"error": "Invalid credentials"}, 
-status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(generics.GenericAPIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({
+                "token": token.key,
+                "user": CustomUserSerializers(user).data
+            })
+
+        return Response(
+            {"error": "Invalid credentials"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
