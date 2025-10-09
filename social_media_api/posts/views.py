39ff09b636d0bfacff426ecfbaefa97121915c5f
Karani <karanik509@gmail.com>
2025-10-09 10:08:03 +0300

    social_media_api

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -47,15 +47,15 @@ class CommentViewSet(viewsets.ModelViewSet):
         instance.delete()
 
 class FeedView(generics.ListAPIView):
-    
+
     serializer_class = PostSerializer
     authentication_classes = [TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
-        followed_users = user.following.all()
-        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
+        following_users = user.following.all()
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
 
 
 
